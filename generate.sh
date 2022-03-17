23f1f09d72030d82193ac04cd8d5d418df07dedf
Dennis Jackson <88591716+dennisjackson@users.noreply.github.com>
2022-03-17 17:55:41 +0000

    Add compression and nested folder support

--- generate.sh
+++ generate.sh
@@ -1,8 +1,9 @@
 TLSSCAN="./tls-scan"
 JQ="jq"
+ZIP="zip"
 DOMAINS=${1:-"domains.txt"}
 FIELDS=".subject, .issuer, .notBefore, .notAfter, .basicConstraints, .sha1Fingerprint"
-OUTDIR=${2:-"out/"`date +\%d\%m\%y-\%H\%M`}
+OUTDIR=${FOODBAR:-"$2/"`date +\%d\%m\%y-\%H\%M`} #Super hacky. Fix.
 
 if [[ ! -f $TLSSCAN ]] 
 then
@@ -15,19 +16,24 @@ if [ ! type "$JQ" &> /dev/null ]; then
     exit -1
 fi
 
+if [ ! type "$ZIP" &> /dev/null ]; then
+    echo "No zip binary at $ZIP."
+    exit -1
+fi
+
 if [ ! -f "$DOMAINS" ]; then
     echo "No domains list at $DOMAINS"
     exit -1
 fi
 
-rm -r $OUTDIR 2&>/dev/null; mkdir $OUTDIR
+rm -r $OUTDIR 2&>/dev/null; mkdir -p $OUTDIR
 
 ERRORS="$OUTDIR/errors.txt"
 RAW="$OUTDIR/raw_data.json"
 
 echo "Fetching results, this may take a while..."
 # Fetch Results
-cat $DOMAINS | $TLSSCAN --port=443 --pretty 2>$ERRORS 1>$RAW
+cat $DOMAINS | $TLSSCAN --port=443 --concurrency=1000 --pretty 2>$ERRORS 1>$RAW
 tail -n 14 $ERRORS
 
 echo ""
@@ -38,33 +44,35 @@ HEADER="host, $FIELDS"
 HEADER=${HEADER//"."/""}
 echo "Fields: $HEADER"
 
-ALL="$OUTDIR/all.csv"
-ROOTS="$OUTDIR/roots.csv"
+#ALL="$OUTDIR/all.csv"
+#ROOTS="$OUTDIR/roots.csv"
 RUSSIA="$OUTDIR/russia.csv"
 
-echo $HEADER > $ALL
-echo $HEADER > $ROOTS
+#echo $HEADER > $ALL
+#echo $HEADER > $ROOTS
 echo $HEADER > $RUSSIA
 
 # All Certificates
-cat $RAW | jq -r '[ [.host] + (.certificateChain | map(['"$FIELDS"']) | .[])] | .[]  | @csv' >> $ALL
+#cat $RAW | jq -r '[ [.host] + (.certificateChain | map(['"$FIELDS"']) | .[])] | .[]  | @csv' >> $ALL
 # All roots 
-cat $RAW | jq -r '[ [.host] +  (.certificateChain | map(select((.basicConstraints | test ("CA:TRUE")))) | map(['"$FIELDS"']) | .[])] | .[]  | @csv' >> $ROOTS
+#cat $RAW | jq -r '[ [.host] +  (.certificateChain | map(select((.basicConstraints | test ("CA:TRUE")))) | map(['"$FIELDS"']) | .[])] | .[]  | @csv' >> $ROOTS
 # All issued by Russia 
 cat $RAW | jq -r '[ [.host] +  (.certificateChain | map(select(.issuer | test ("The Ministry of Digital Development and Communications"))) | map(['"$FIELDS"']) | .[])] | .[]  | @csv' >> $RUSSIA
 
+ZIPNAME="$OUTDIR/raw.zip"
+$ZIP --junk-paths $ZIPNAME $RAW $ERRORS $DOMAINS 
+rm $RAW
+rm $ERRORS
+
 echo ""
 echo "Output:"
-echo $ALL
-echo $ROOTS
+#echo $ALL
+#echo $ROOTS
 echo $RUSSIA 
 echo ""
 echo "Working State:"
-echo  $ERRORS
-echo $RAW
+echo $ZIPNAME
 
-cp $DOMAINS $OUTDIR/domains.txt
-
-LATEST=out/latest
+LATEST="$2/latest"
 rm -r $LATEST 2&>/dev/null; mkdir $LATEST
 cp -r $OUTDIR/* $LATEST
