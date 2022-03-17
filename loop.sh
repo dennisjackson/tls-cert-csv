ee772a1c4fca6f81ba9eb5bfa5a4d816a2f555ba
Dennis Jackson <88591716+dennisjackson@users.noreply.github.com>
2022-03-17 18:52:54 +0000

    Add targetted

--- loop.sh
+++ loop.sh
@@ -3,7 +3,11 @@ while true; do
     ./generate.sh lists/latest-censys.txt html/censys
     echo "### gosuslugi:"
     ./generate.sh lists/latest-gosuslugi.txt html/gosuslugi
-    echo "### tranco:"
-    ./generate.sh lists/tranco_russia_10k.txt html/tranco
+    echo "### bespoke:"
+    ./generate.sh lists/latest-bespoke.txt html/bespoke
+    echo "### tranco10k:"
+    ./generate.sh lists/tranco_russia_10k.txt html/tranco_10k
+    echo "### tranco100k:"
+    ./generate.sh lists/tranco_russia_100k.txt html/tranco_100k
     sleep 3600
 done
