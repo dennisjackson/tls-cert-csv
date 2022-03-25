a5344fc30219435a1f2b9a2c88c5b7a84754719e
Dennis Jackson <88591716+dennisjackson@users.noreply.github.com>
2022-03-25 10:54:14 +0000

    Remove whitespace from censys lists

--- filter-censys.sh
+++ filter-censys.sh
@@ -1,6 +1,7 @@
 cat $1 | jq -r '[ .results | map( [.parsed | .names ] ) | flatten(2) | .[]  ]' | \
     sed "s/\"//g"  | sed "s/null,//" | sed "s/,//" | sed "s/\[//" | sed "s/\]//" | \
     sed "s/\*.//" | \
+    tr -d " " | \
     tr '[:upper:]' '[:lower:]' | \
     awk 'NF' | \
     uniq
