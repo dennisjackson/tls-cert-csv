94870e1bfd924ba204a133d04e6949e06faa88cf
Dennis Jackson <88591716+dennisjackson@users.noreply.github.com>
2022-03-27 18:45:35 +0100

    Finish automating filtering of censys list

--- filter-censys.sh
+++ filter-censys.sh
@@ -1,3 +1,8 @@
+OUTDIR="lists"
+PREFIX=`date +\%d\%m\%y-\%H\%M-censys`
+FILTERED_LIST="$OUTDIR/$PREFIX.txt" 
+LATEST="$OUTDIR/latest-censys.txt"
+TEMP="filtered.txt"
 cat $1 | jq -r '[ .results | map( [.parsed | .names ] ) | flatten(2) | .[]  ]' | \
     sed "s/\"//g"  | sed "s/null,//" | sed "s/,//" | sed "s/\[//" | sed "s/\]//" | \
     sed "s/\*.//" | \
@@ -5,5 +10,15 @@ cat $1 | jq -r '[ .results | map( [.parsed | .names ] ) | flatten(2) | .[]  ]' |
     tr '[:upper:]' '[:lower:]' | \
     awk 'NF' | \
     sort | \
-    uniq 
+    uniq > $TEMP 
 
+cp $TEMP $LATEST
+cp $TEMP $FILTERED_LIST
+
+git add $LATEST
+git add $FILTERED_LIST
+rm $TEMP 
+
+#git commit -m "Update to latest censys list for `date +\%d-\%m-\%y`"
+
+ 
