7ce6b19d3e63768b80e76eaa618689399a302ffe
Dennis Jackson <88591716+dennisjackson@users.noreply.github.com>
2022-10-23 20:12:14 +0100

    Add selected entities list to loop

--- loop.sh
+++ loop.sh
@@ -1,11 +1,13 @@
 while true; do
-    git pull 
+    git pull
     echo "### censys:"
     ./generate.sh lists/latest-censys.txt html/censys
     echo "### gosuslugi:"
     ./generate.sh lists/latest-gosuslugi.txt html/gosuslugi
     echo "### bespoke:"
     ./generate.sh lists/latest-bespoke.txt html/bespoke
+    echo "### selected-entities:"
+    ./generate.sh lists/latest-selected-entities.txt html/selected_entities
     echo "### tranco-10k:"
     ./generate.sh lists/tranco_russia_10k.txt html/tranco_10k
     echo "### tranco-100k:"
